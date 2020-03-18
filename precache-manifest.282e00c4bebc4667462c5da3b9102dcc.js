b61368d7073da7d5bd93f398f40b1ef4d11c79f1
yushaer <yushaefraza@gmail.com>
2020-03-17 22:40:49 -0600

    Add files via upload

---
+++ precache-manifest.282e00c4bebc4667462c5da3b9102dcc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da939df20ee70069c4dc43cd34ed32f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79553f141fa53e50db68",
+    "url": "/static/css/main.2b25efda.chunk.css"
+  },
+  {
+    "revision": "878fd4546410f5c75120",
+    "url": "/static/js/2.5efad354.chunk.js"
+  },
+  {
+    "revision": "19d24ae19ee06c3a97936261381c1fb8",
+    "url": "/static/js/2.5efad354.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79553f141fa53e50db68",
+    "url": "/static/js/main.3eb0bda9.chunk.js"
+  },
+  {
+    "revision": "d42a603f0f0be8948605",
+    "url": "/static/js/runtime-main.d303fdb9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
