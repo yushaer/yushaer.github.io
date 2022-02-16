b98f6e5ba04ed56819e29daf1c928ccb6ce6f133
yushaer <yushaefraza@gmail.com>
2022-02-16 16:25:58 -0500

    Fixed-routing

---
+++ pokemon-team-builder/precache-manifest.976adf63ee56e825f6f4911827bc6f5e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aab20c07c3081917817c53de79d037d3",
+    "url": "/pokemon-team-builder/index.html"
+  },
+  {
+    "revision": "5240e668731f9464fd79",
+    "url": "/pokemon-team-builder/static/css/main.e3d966c8.chunk.css"
+  },
+  {
+    "revision": "638048c22e9c364c3098",
+    "url": "/pokemon-team-builder/static/js/2.565ea1d6.chunk.js"
+  },
+  {
+    "revision": "29bdfc77c34bc7519cf0de138a152f24",
+    "url": "/pokemon-team-builder/static/js/2.565ea1d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5240e668731f9464fd79",
+    "url": "/pokemon-team-builder/static/js/main.fd941f85.chunk.js"
+  },
+  {
+    "revision": "cbb14ba518b16122c9aa",
+    "url": "/pokemon-team-builder/static/js/runtime-main.3ac58afd.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/pokemon-team-builder/static/media/logo.ee7cd8ed.svg"
+  }
+]);
--- pokemon-team-builder/service-worker.js
+++ pokemon-team-builder/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pokemon-team-builder/precache-manifest.a194f3de5ae6e9f8bd69d5cf6747e348.js"
+  "/pokemon-team-builder/precache-manifest.976adf63ee56e825f6f4911827bc6f5e.js"
 );
 
 self.addEventListener('message', (event) => {
